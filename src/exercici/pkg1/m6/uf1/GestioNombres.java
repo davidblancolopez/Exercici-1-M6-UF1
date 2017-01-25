454505ec2dd760ee52caf9a7250781ddf4d5b3ad
davidblancolopez <daviddbl94@gmail.com>
2017-01-25 16:33:18 +0100

    CAMBIOS

--- src/exercici/pkg1/m6/uf1/GestioNombres.java
+++ src/exercici/pkg1/m6/uf1/GestioNombres.java
@@ -5,21 +5,30 @@ import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
 
 public class GestioNombres {
+
     File fitxer = new File("C:\\proves\\Execici1");
-   
 
     /**
-     * Generar el  següent nombre primer, a continuació de l'últim emmagatzemat en un fitxer i afegir-lo a aquest.
-     * @throws FileNotFoundException 
+     * Generar el següent nombre primer, a continuació de l'últim emmagatzemat
+     * en un fitxer i afegir-lo a aquest.
+     *
+     * @throws FileNotFoundException
      */
     public void generarNextNombre() throws FileNotFoundException {
-         RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         try {
             if (fitxer.length() == 0) {
 
             } else {
+                boolean seguir = true;
+                
+                raf.seek(fitxer.length() - 4);
+                int ultimNum = raf.readInt();
+                
                 
             }
+
+            raf.close();
         } catch (Exception e) {
 
         }
@@ -27,41 +36,43 @@ public class GestioNombres {
 
     /**
      * Poder obtenir el nombre de primer que ocupa una determinada posició.
+     *
      * @param posicio
      * @return
-     * @throws FileNotFoundException 
+     * @throws FileNotFoundException
      */
     public int numeroEnPosicio(int posicio) throws FileNotFoundException {
         RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         int numPrim = 0;
 
         for (int i = 0; i < (posicio - 1); i++) {
-
+            raf.readInt(fitxer);
         }
 
         return numPrim;
     }
-    
+
     /**
      * Poder conèixer quants nombres primers hi ha emmagatzemats.
+     *
      * @return
-     * @throws FileNotFoundException 
+     * @throws FileNotFoundException
      */
-    public int numerosPrimsTotal() throws FileNotFoundException{
+    public int numerosPrimsTotal() throws FileNotFoundException {
         RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         int total = 0;
-        
-        
+
         return total;
     }
-    
+
     /**
      * Quin és el valor de l'últim emmagatzemat.
-     * @return 
+     *
+     * @return
      */
-    public int ultimNumeroPrim(){
+    public int ultimNumeroPrim() {
         int ultimPrim = 0;
-        
+
         return ultimPrim;
     }
 }
