c4d83f79ecc1da4845b39e5abe3ddd0451314267
davidblancolopez <daviddbl94@gmail.com>
2017-01-25 16:23:09 +0100

    CAMBIOS

---
+++ src/exercici/pkg1/m6/uf1/Exercici1M6UF1.java
@@ -0,0 +1,19 @@
+
+package exercici.pkg1.m6.uf1;
+
+import java.io.File;
+
+
+public class Exercici1M6UF1 {
+
+
+    public static void main(String[] args) {
+        File fitxer = new File("C:\\proves\\Execici1", "rw");
+        GestioNombres nombres = new GestioNombres();
+        nombres.generarNextNombre();
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+    
+}
---
+++ src/exercici/pkg1/m6/uf1/GestioNombres.java
@@ -0,0 +1,67 @@
+package exercici.pkg1.m6.uf1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+public class GestioNombres {
+    File fitxer = new File("C:\\proves\\Execici1");
+   
+
+    /**
+     * Generar el  següent nombre primer, a continuació de l'últim emmagatzemat en un fitxer i afegir-lo a aquest.
+     * @throws FileNotFoundException 
+     */
+    public void generarNextNombre() throws FileNotFoundException {
+         RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
+        try {
+            if (fitxer.length() == 0) {
+
+            } else {
+                
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    /**
+     * Poder obtenir el nombre de primer que ocupa una determinada posició.
+     * @param posicio
+     * @return
+     * @throws FileNotFoundException 
+     */
+    public int numeroEnPosicio(int posicio) throws FileNotFoundException {
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
+        int numPrim = 0;
+
+        for (int i = 0; i < (posicio - 1); i++) {
+
+        }
+
+        return numPrim;
+    }
+    
+    /**
+     * Poder conèixer quants nombres primers hi ha emmagatzemats.
+     * @return
+     * @throws FileNotFoundException 
+     */
+    public int numerosPrimsTotal() throws FileNotFoundException{
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
+        int total = 0;
+        
+        
+        return total;
+    }
+    
+    /**
+     * Quin és el valor de l'últim emmagatzemat.
+     * @return 
+     */
+    public int ultimNumeroPrim(){
+        int ultimPrim = 0;
+        
+        return ultimPrim;
+    }
+}
